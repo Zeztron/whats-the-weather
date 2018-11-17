b93a74b1ddef7aff0269300223a6cd94208d0752
Zeztron <pateltharsh@gmail.com>
2018-11-17 18:52:45 -0500

    Refactored data loading into lifecycle methods

--- src/index.js
+++ src/index.js
@@ -7,26 +7,24 @@ class App extends React.Component {
         super(props);
 
         this.state = {lat: null, errorMessage: ""};
-
+    }
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                // We called setState!
-                this.setState({lat: position.coords.latitude});
-            },
-            err => {
-                this.setState({errorMessage: err.message});
-            }
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
         );
     }
     // React says we HAVE to define render!!
     render() {
-        return (
-            <div>
-                Latitude: {this.state.lat}
-                <br />
-                Error: {this.state.errorMessage}
-            </div>
-        );
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+
+        return <div>Loading!</div>;
     }
 }
 

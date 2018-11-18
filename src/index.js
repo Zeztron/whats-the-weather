7b7dd24a086c63b6df6f3909e62e5c3e6b9bdbbd
Zeztron <pateltharsh@gmail.com>
2018-11-17 20:07:51 -0500

    Added season display

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = {lat: null, errorMessage: ""};
 
-        this.state = {lat: null, errorMessage: ""};
-    }
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             position => this.setState({lat: position.coords.latitude}),
@@ -21,10 +18,10 @@ class App extends React.Component {
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />;
         }
 
-        return <div>Loading!</div>;
+        return <div>Loading...</div>;
     }
 }
 
